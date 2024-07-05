c8d8a47755d4b7b3404b8caa7d2e7f3b72ddfe18
Vikas Wakde <110342308+vikaswakde@users.noreply.github.com>
2024-07-06 02:06:18 +0530

    day-3 feedback card design complete

--- src/app/frontend-mentor-2/page.tsx
+++ src/app/frontend-mentor-2/page.tsx
@@ -1,10 +1,25 @@
+"use client";
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
+import React, { useState } from "react";
 
-const page = () => {
-  return (
+const ChallengeTwo = () => {
+  const [selectedRating, setSelectedRating] = useState<number>();
+  const [isSubmited, setIsSubmitted] = useState(false);
+
+  const handleSelectedRating = (rating: number) => {
+    setSelectedRating(rating);
+  };
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    setIsSubmitted(true);
+  }
+  return !isSubmited ? (
     <div className="flex items-center justify-center min-h-screen">
-      <form className="bg-red-500 w-[25rem] h-[25rem] rounded-3xl flex flex-col items-center justify-around">
+      <form
+        className="bg-gradient-to-tr from-dark-blue to-dark-blue-default w-[25rem] h-[25rem] rounded-3xl flex flex-col items-center justify-around"
+        onSubmit={handleSubmit}
+      >
         <div className="w-full pl-6 pt-4 -mb-2">
           <div className="bg-green-900 w-fit p-3 rounded-full">
             <img
@@ -19,22 +34,45 @@ const page = () => {
           Please let us know how we did with your support request. All feedback
           is appreciated to help us improve our offering!
         </p>
-        <ul className="flex w-full justify-around items-center h-12 px-5">
-          {[1, 2, 3, 4, 5].map((num) => (
-            <li
-              className="bg-green-900 w-8 rounded-full h-8 p-6 flex items-center justify-center active:text-black active:bg-white hover:bg-orange-500 text-white "
-              key={num}
+        <div className="flex w-full justify-around items-center h-12 px-5">
+          {[1, 2, 3, 4, 5].map((rating) => (
+            <button
+              onClick={() => handleSelectedRating(rating)}
+              className="bg-gray-600 shadow-slate-500 w-8 rounded-full h-8 p-6 flex items-center justify-center active:text-black active:bg-white hover:bg-orange-500 text-white "
+              key={rating}
+              type="button"
             >
-              {num}
-            </li>
+              {rating}
+            </button>
           ))}
-        </ul>
-        <button className="w-[85%] bg-cyan-800 rounded-full h-10">
+        </div>
+        <button
+          // disabled={selectedRating == undefined}
+          className="w-[85%] bg-cyan-800 rounded-full h-10"
+        >
           Submit
         </button>
       </form>
     </div>
+  ) : (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="bg-gradient-to-tr from-dark-blue to-dark-blue-default w-[25rem] h-[25rem] rounded-3xl flex flex-col items-center justify-around text-center">
+        <img
+          src="./assets/images/illustration-thank-you.svg"
+          className="mt-4 h-23 -mb-3"
+          alt="icon-thank-you"
+        />
+        <div className="bg-gray-600 text-blue-300 px-4 py-2 rounded-full -mb-4">
+          <p>You selected {selectedRating} out of 5</p>
+        </div>
+        <h1 className="pl-5 text-2xl font-bold -mb-4">Thank You!</h1>
+        <p className="px-8 text-sm -mt-6 pb-5 text-gray-300">
+          We appreciate you taking the time to give a rating. If you ever need
+          more support, don’t hesitate to get in touch!
+        </p>
+      </div>
+    </div>
   );
 };
 
-export default page;
+export default ChallengeTwo;
